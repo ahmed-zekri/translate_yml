822ec397a1dd87f5ec6635491cafeb8fd7291f8c
ahmed103 <ahmed.zekri.a@gmail.com>
2021-05-12 15:12:09 +0100

    multithreading

--- translate.py
+++ translate.py
@@ -1,15 +1,20 @@
 import argparse
 import subprocess
+import concurrent.futures
 
 
-def translate_value(value):
+def translate_value(value, sub_key, key, sub_sub_key=None):
     global counter
+    global parsed_yaml
     counter += 1
     print(f'translating {counter} of {all_items}')
     value = translator.translate(value, lang_tgt='fr')
     value = str(value).replace('_char_', '%') if type(value) is str else value[0]
     print(value)
-    return value
+    if sub_sub_key is None:
+        parsed_yaml[key][sub_key] = value
+    else:
+        parsed_yaml[key][sub_key][sub_sub_key] = value
 
 
 def count_yml(parsed_yaml):
@@ -35,23 +40,24 @@ if __name__ == '__main__':
 
     with open(file='messages.th-10-05-2021-2.yml', mode='r', encoding='utf-8') as file:
         content = file.read()
-        parsed_yaml_output = dict()
+
         parsed_yaml = yaml.safe_load(content.replace('%', '_char_'))
 
         all_items = count_yml(parsed_yaml)
 
         counter = 0
-        for key, value in parsed_yaml.items():
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for key, value in parsed_yaml.items():
 
-            for sub_key, sub_value in value.items():
+                for sub_key, sub_value in value.items():
 
-                if type(value[sub_key]) is not dict:
-                    value[sub_key] = translate_value(value[sub_key])
+                    if type(value[sub_key]) is not dict:
+                        executor.submit(translate_value, value[sub_key], sub_key, key)
 
-                else:
-                    for sub_sub_key, sub_sub_value in value[sub_key].items():
-                        value[sub_key][sub_sub_key] = translate_value(value[sub_key][sub_sub_key])
+                    else:
+                        for sub_sub_key, sub_sub_value in value[sub_key].items():
+                            executor.submit(translate_value, value[sub_key], sub_key, key, sub_sub_key=sub_sub_key)
 
-            parsed_yaml_output[key] = value
+                # parsed_yaml_output[key] = value
     with open(file='translated.yml', mode='w') as file:
-        yaml.safe_dump(parsed_yaml_output, file, encoding='utf-8')
+        yaml.safe_dump(parsed_yaml, file, encoding='windows-1252',allow_unicode=True)
